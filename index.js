f720c8f253aae88f9e149401fbfb8bdf66f96596
Alice Genkin <agenkin1@gmail.com>
2021-09-21 16:27:38 -0400

    commit tree 53971bf57b2b9582930c16be2acaa2799e72d179

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
 
-// Write your code here
+let num3 = 18;
+let num4 = 14;
+
+let mod = num3 % num4;
+let max = Math.max (2,3,4,20);
